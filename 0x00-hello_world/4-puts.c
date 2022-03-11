11fe65fd7644b8adf402825176db94649da82127
lamecksingu <singulameck9619@gmail.com>
2022-03-11 05:13:49 -0800

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  *  Return: Always 0 (success)
  */
-int main(void)
+int main(void) //Main function
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
