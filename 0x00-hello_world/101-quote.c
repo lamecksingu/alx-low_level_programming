081000b736f4d5189c9cfb71109b35fde7087f43
lamecksingu <singulameck9619@gmail.com>
2022-03-12 02:27:16 -0800

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 
 /**
  *  main - -Entry point
  *
- *  Return: return 1 success
+ *  Return: return (1) success
  */
 int main(void)
 {
-	write(STDOUT_FILENO, and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n);
+	write(STDOUT_FILENO, and that piece of art is useful);
+	write(STDOUT_FILENO, " - Dora Korpar, 2015-10-19\n);
 	return (1);
 }
