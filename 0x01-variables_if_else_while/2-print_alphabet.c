622c0e083895117db707e070c14a4255ad871aa2
lamecksingu <singulameck9619@gmail.com>
2022-03-12 13:35:09 -0800

    Prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always (success)
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c\n);
+	}
+	return (0);
+}
