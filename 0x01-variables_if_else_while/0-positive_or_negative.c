ff06efb4f2cc667f50261a9d952f72f710525de6
lamecksingu <singulameck9619@gmail.com>
2022-03-12 08:05:08 -0800

    Variables

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ *  main -Entry point
+ *
+ *  return: Return (0) Always success
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your codes goes there */
+	return (0);
+}
