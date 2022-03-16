7dc7ffe4a1df00469e5a88975b3e14afc1fe73f8
lamecksingu <99335631+lamecksingu@users.noreply.github.com>
2022-03-16 22:42:26 +0300

    Main.h for Print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,2 @@
-#include<stdlib.h>
-#include<stdio.h>
-/*char _putchar(char i)
-{
-	printf('%c', i);
-
-
-}
-*
-* print_alphabet - a function to  print alphabets
-*/
-void print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-		putchar('\n');
-	}
-}
+int _putchar(char c);
+void print_alphabet(void);
