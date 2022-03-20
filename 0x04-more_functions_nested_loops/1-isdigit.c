e148236a8e9a99a06ec37d9b08e95f53db274e7f
lamecksingu <99335631+lamecksingu@users.noreply.github.com>
2022-03-20 14:39:45 +0300

    Checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for a digit between 0-9
+ *
+ * @c: interger to check digit
+ *
+ * Return: Always o (Success)
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		return (1);
+	}
+	else
+		return (0);
+	return (0);
+}
