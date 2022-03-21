e34ac215d4ef89d0dd7626ddf8e20ba5fae10bf6
lamecksingu <99335631+lamecksingu@users.noreply.github.com>
2022-03-21 17:07:39 +0300

    A function to reset a variable's value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function to reset a variable's value to 98
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	int *p = &n;
+
+	*p = 98;
+}
