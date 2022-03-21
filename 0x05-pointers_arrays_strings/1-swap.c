1f01ba481afef4ec15d500d04aad50bfc9c96eba
lamecksingu <99335631+lamecksingu@users.noreply.github.com>
2022-03-21 17:57:32 +0300

    Function to swap values of two intergers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int - Swap the values of two intergers
- *
- * @a: variable a
- * @b: variable b
- *
+ * swap_int - swaps the values of two integers
+ * @a: pointer 1
+ * @b: pointer 2
  * Return: void
  */
+
 void swap_int(int *a, int *b)
 {
-	*a = &b;
-	*b = &a;
+	int x = *a;
+	*a = *b;
+	*b = x;
 }
