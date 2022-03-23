5a33edced10b9f98596947b6bb1da2d9db9d81e1
lamecksingu <99335631+lamecksingu@users.noreply.github.com>
2022-03-23 18:23:22 +0300

    Concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - a fuction to concatenate strings
+ *
+ * @dest: a destination variable
+ * @src: a source variable
+ *
+ * @i: Interger for iteration
+ * @j: interger for iteration
+ *
+ * Return: a concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return dest;
+}
