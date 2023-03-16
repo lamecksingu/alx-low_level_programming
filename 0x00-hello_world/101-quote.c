35e61f487d03f2e269412087f1fcb389de32108b
lamecksingu <singulameck9619@gmail.com>
2023-03-16 21:39:06 +0300

    More changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include <unistd.h>
 #include<stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-/**
- * main - Entry point
- *
- * Return: 1 Success
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+int main(void) {
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int fd = open("/dev/stderr", O_WRONLY);
+    write(fd, message, sizeof(message)-1);
+    close(fd);
+    return 1;
 }
