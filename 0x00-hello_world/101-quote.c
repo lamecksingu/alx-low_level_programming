f1c3e5d71d0c9c5e6865aed8ab9732253b274065
lamecksingu <singulameck9619@gmail.com>
2023-03-16 21:56:39 +0300

    Betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(void) {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int fd = open("/dev/stderr", O_WRONLY);
-    write(fd, message, sizeof(message)-1);
-    close(fd);
-    return 1;
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	const char message[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
+
+	int fd = open("/dev/stderr", O_WRONLY);
+	write(fd, message, sizeof(message)-1);
+	close(fd);
+	return (1);
 }
