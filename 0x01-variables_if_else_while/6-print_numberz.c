8615403bb917e5de19f41e56f480c53b076ea3ea
lamecksingu <singulameck9619@gmail.com>
2023-03-17 20:46:10 +0300

    Character to numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
