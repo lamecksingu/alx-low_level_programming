01837a2841437587d65b368a4bbd1139301b44b5
lamecksingu <singulameck9619@gmail.com>
2023-03-21 18:24:58 +0300

    Fix putchar definition

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
+ * @c: the character to print
  *
- * @c: The character to print
- *
- * Return: Character passed to it
+ * write - function returned value
+ * Return: On success 1
+ * On error, -1 is returned
  */
 int _putchar(char c)
 {
