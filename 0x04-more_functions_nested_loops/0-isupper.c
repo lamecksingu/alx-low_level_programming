63e47b5b2ca8df2e71f7d6bd15e204d1d72e3def
lamecksingu <singulameck9619@gmail.com>
2023-03-23 18:14:10 +0300

    Modify main.h file with putchar

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * isupper - checks if a character is an uppercas
+ * _isupper - checks if a character is an uppercas
  * @c: a character to be checked
+ *
+ * Return: 0
  */
 
 int _isupper(int c)
@@ -10,9 +12,10 @@ int _isupper(int c)
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
-	} else if ( c >= 97 && c <= 122)
+	} else if (c >= 97 && c <= 122)
 	{
 		return (0);
 	}
+	_putchar('\n');
 	return (0);
 }
