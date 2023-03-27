2ac2e5ffb7bb589b8feb0b71902ee1842ff34018
lamecksingu <singulameck9619@gmail.com>
2023-03-27 18:10:11 +0300

    Fix logic in array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,26 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * print_array - prints the specifie number of
- * array elements
+ * print_array - function that prints n elements of an array of integers,
+ * followed by a new line.
+ * @a: This is the input array
+ * @n: This is the lenght of the array
  *
- * @a: pointer to array
- * @n: number of values to be printed
+ *
  */
 
 void print_array(int *a, int n)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
-		printf("%d", a[i]);
-		if (i != n - 1)
+		printf("%d", a[index]);
+		if (index != (n - 1))
 		{
-			printf(",");
+			printf(", ");
 		}
 	}
-	printf("\n");
+	putchar('\n');
 }
