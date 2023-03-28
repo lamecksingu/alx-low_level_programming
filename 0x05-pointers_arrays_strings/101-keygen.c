5d8dce2b16128af4170beeb64d1e43b379c18bae
lamecksingu <singulameck9619@gmail.com>
2023-03-28 15:32:16 +0300

    A password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MIN_LENGTH 6
+#define MAX_LENGTH 12
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int length = rand() % (MAX_LENGTH - MIN_LENGTH + 1) + MIN_LENGTH;
+	char password[length + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	srand(time(NULL));
+
+	for (int i = 0; i < length; i++)
+	{
+		password[i] = charset[rand() % (sizeof(charset) - 1)];
+	}
+	password[length] = '\0';
+	printf("Password for 101-crackme: %s\n", password);
+	return (0);
+}
