555529637a713c277d481a0493ec1eee974aa763
lamecksingu <singulameck9619@gmail.com>
2023-03-29 17:55:34 +0300

    Add readme file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function to reverse array
+ * of integers
+ * @a: the array to be reversed
+ * @n: Size of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 1;
+
+	while (n != 0)
+	{
+		a[n - 1];
+		i++;
+		n--;
+	}
+}
