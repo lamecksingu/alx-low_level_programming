2d9666c1149945ab3ad7ac3fb3d752234c218c1c
lamecksingu <singulameck9619@gmail.com>
2023-04-18 20:15:52 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure to define data type dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: Owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
