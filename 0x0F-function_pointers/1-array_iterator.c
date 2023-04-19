2d2de312834a63d6d91b35957d9d3a5bf9fab98a
lamecksingu <singulameck9619@gmail.com>
2023-04-19 22:36:36 +0300

    Change the logic check for NULL occurance

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+	
+	if (array != NULL && action != NULL && size > 0)
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 }
