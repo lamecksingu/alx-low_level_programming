47a4d3ec4449b608f8db0a180adbea614db034d3
lamecksingu <singulameck9619@gmail.com>
2023-07-06 09:57:26 -0700

    Removing segmantation fault

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * wildcmp - a function that compares two strings
  * @s1: first string
@@ -8,16 +9,38 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (0);
+	}
 	if (*s1 == '\0' && *s2 == '\0')
 	{
 		return (1);
 	}
-	if (*s2 == '*' || (*s1 == *s2 && *s1 != '\0'))
+	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-		{
-			return (1);
-		}
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
+/**
+ *	if (*s1 == '\0' && *s2 == '\0')
+ *	{
+ *	return (1);
+ *	}
+ *	if (*s2 == '*' || (*s1 == *s2 && *s1 != '\0'))
+ *	{
+ *	if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+ *	{
+ *	return (1);
+ *	}
+ *	}
+ *	return (0);
+ *	}
+ */
+
+
