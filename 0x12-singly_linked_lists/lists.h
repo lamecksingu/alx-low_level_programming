9b074982aeb46ad6ac1eed016f21f4fa62f6977c
lamecksingu <singulameck9619@gmail.com>
2023-07-27 05:14:53 -0700

    A function to add a node at the beggining of a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,5 +19,6 @@ typedef struct list_s
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
