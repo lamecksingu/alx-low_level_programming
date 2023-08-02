31ca625dc3dd5825df374cece9c7f9d1623bbbe0
lamecksingu <singulameck9619@gmail.com>
2023-08-02 11:51:52 -0700

    Free list safe

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,16 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
@@ -27,5 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 
 #endif
