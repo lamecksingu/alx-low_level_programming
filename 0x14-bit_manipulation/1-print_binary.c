26c33e54bac76f56cbc52a9e2613695888103d0a
lamecksingu <singulameck9619@gmail.com>
2023-08-08 05:28:02 -0700

    A new approach to printing binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,23 +5,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int size = sizeof(unsigned long int) * 8;
-	unsigned long int mask = 1UL << (size - 1);
-	int leading_zeros = 1;/*to skip leadingzeros*/
-	unsigned long int i;
-
-	for (i = 0; i < size; i++)
+	if (n > 1)
 	{
-		if ((n & mask) || !leading_zeros)
-		{
-			leading_zeros = 0;
-			printf("%c", (n & mask) ? '1' : '0');
-		}
-		mask >>= 1;
+		print_binary(n >> 1);
 	}
-	/*in case the number was zero*/
-	if (leading_zeros)
-	{
-		printf("0");
-	}
+	_putchar((n & 1) ? '1' : '0');
 }
