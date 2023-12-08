62d66aebaa803a3658de64e74f5e3e34ed476ad3
lamecksingu <singulameck9619@gmail.com>
2023-12-08 13:46:21 +0300

    Correcting to follow betty coding style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_dlistint - a function to print elements of a doubly linked list
+ * @h: pointer to the elements in a structure
+ * Return: Elements of a list
+ */
 
-size_t print_dlistint(const dlistint_t *h) {
+size_t print_dlistint(const dlistint_t *h)
+{
 	size_t nodes = 0;
 
-	while (h != NULL) {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		nodes++;
 		h = h->next;
 	}
 
-	return nodes;
+	return (nodes);
 }
 
